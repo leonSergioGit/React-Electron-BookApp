7af3bf4f46e82a6e864852a82b2cc868d0017f48
unknown <leongit1990@gmail.com>
2020-02-10 23:00:21 +0100

    Form validation using useState and useEffect hooks

--- src/components/pages/AddBookModal.js
+++ src/components/pages/AddBookModal.js
@@ -1,5 +1,6 @@
-import React, { useState, useContext, useEffect } from 'react';
+import React, { Fragment, useState, useContext, useEffect } from 'react';
 import Book from '../model/Book';
+import Warning from "./Warning";
 import BookContext from '../context/bookContext';
 import { uuid } from 'uuidv4';
 
@@ -33,10 +34,11 @@ const useStyles = makeStyles(theme => ({
 }));
 
 
+
 // MODAL FUNCTION
 const TransitionsModal = () => {
 
-  // Use state hook. A way to manipulate state from a functional component
+  // UseState hook. A way to manipulate state from a functional component
   const [books, setBook] = useState({
       id: '',
       name: '',
@@ -46,33 +48,78 @@ const TransitionsModal = () => {
       finished: false 
   });
 
+  //UseState hooks to check input validation
+  const [validName, setValidName] = useState(false);
+  const [validAuthor, setValidAuthor] = useState(false);
+  const [validLanguage, setValidLanguage] = useState(false);
+  const [validDate, setValidDate] = useState(false);
+
+
+ 
   // Context to access to the global state
   const bookContext =  useContext(BookContext);
   const { addBooks, getBooks } = bookContext;
 
- 
+  //Use effect runs after every render
+  //We pass as array values the state that we want useEffect to check
+  useEffect(() => {
+    saveData();
+  }, [validName, validAuthor, validLanguage, validDate]);
+
   // Submit function
   const onSubmit = e =>  {
     e.preventDefault();
-    addBooks(new Book(uuid(), books.name, books.language, books.author, books.date, books.finished));
-    console.log(books)
-    setBook({
-      id: '',
-      name: '',
-      language: '',
-      author: '',
-      date: '',
-      finished: false
-    })
 
-    fs.appendFile('books.txt', `${books.name};${books.author};${books.language};${books.date};${books.finished}\n`, function (err) {
-      if (err) throw err;
-      console.log('Saved!');
-    });
+      if(books.name.trim() == ""){
+        setValidName(false);
+      } else {
+        setValidName(true);
+      }
+
+      if(books.author.trim() == ""){
+        setValidAuthor(false);
+      } else {
+        setValidAuthor(true);
+      }
+      if(books.language.trim() == ""){
+        setValidLanguage(false);
+      } else {
+        setValidLanguage(true);
+      }
+
+      if(books.date.trim() == ""){
+        setValidDate(false);
+      } else {
+        setValidDate(true);
+      }
+    } 
+  
+    //Function that save our book in a file and updates our state
+   const saveData = () => {
+    if(validName == true && validAuthor == true && validLanguage == true && validDate == true){
+      addBooks(new Book(uuid(), books.name, books.language, books.author, books.date, books.finished));
+      setBook({
+        id: '',
+        name: '',
+        language: '',
+        author: '',
+        date: '',
+        finished: false
+      })
+  
+      fs.appendFile('books.txt', `${books.name};${books.author};${books.language};${books.date};${books.finished}\n`, function (err) {
+        if (err) throw err;
+        console.log('Saved!');
+      });
+
+      handleClose();
+    }
   }
 
   //On change function to update this component's state with the new book
-  const onChange = e => setBook({ ...books, [e.target.name]: e.target.value });
+  const onChange = e => {
+    setBook({ ...books, [e.target.name]: e.target.value })
+  };
 
   // Current date
   const date = new Date;
@@ -82,7 +129,7 @@ const TransitionsModal = () => {
   const dateCompleted = `${year}-${month}-${day}`;
 
 
-  // Material-UI functionality
+  // Material-UI functionality and MODAL
   const classes = useStyles();
   const [open, setOpen] = useState(false);
 
@@ -93,6 +140,8 @@ const TransitionsModal = () => {
   const handleClose = () => {
     setOpen(false);
   };
+
+
 
   return (
     <div>
@@ -116,6 +165,7 @@ const TransitionsModal = () => {
           <div className={classes.paper}>
             <form className={classes.root} noValidate autoComplete="off">
               <div className='inputText'>
+                <Warning message={validName ? "" : "Add a book name"} />
                 <TextField 
                   id="standard-basic" 
                   label="Book name"
@@ -125,6 +175,7 @@ const TransitionsModal = () => {
                 />
               </div>
               <div className='inputText'>
+              <Warning message={validAuthor ? "" : "Add an author"} />
                 <TextField 
                   id="standard-basic2" 
                   label="Language"
@@ -134,6 +185,7 @@ const TransitionsModal = () => {
                 />
               </div>
               <div className='inputText'>
+              <Warning message={validLanguage ? "" : "Add a language"} />
                 <TextField 
                   id="standard-basic3" 
                   label="name"
@@ -143,6 +195,7 @@ const TransitionsModal = () => {
                 />
               </div>
               <div>
+              <Warning message={validDate ? "" : "Add a date"} />
                 <TextField
                   id="date"
                   label="Date"
@@ -157,11 +210,10 @@ const TransitionsModal = () => {
                 />
               </div>
               <div>
-                <span>Finished: </span>
                 <label htmlFor="">Yes</label>      
                 <input type="radio" name="finished" value={true} onChange={onChange}/>
                 <label htmlFor="">No</label>
-                <input type="radio" name="finished" value={false} onChange={onChange}/>
+                <input type="radio" name="finished" value={false} checked onChange={onChange}/>
               </div>
               <Button 
                 variant="contained" 
---
+++ src/components/pages/Warning.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Warning(props) {
+    return (
+        <div>
+            {props.message}
+        </div>
+    )
+}
+
+
+export default Warning;
